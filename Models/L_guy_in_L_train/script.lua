2e0d87305cc5a7abfe51ee189db0203ac96625cb
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2022-04-13 19:14:35 -0400

    some changes uwu
    modified the origins on some of the bones of the model

--- Models/L_guy_in_L_train/script.lua
+++ Models/L_guy_in_L_train/script.lua
@@ -1,3 +1,7 @@
-for key, value in pairs(vanilla_model) do
-    value.setEnabled(false)
+--for key, value in pairs(vanilla_model) do
+--    value.setEnabled(false)
+--end
+
+function render(delta)
+    model.NO_PARENT_Train.setPos(vectors.worldToPart(player.getPos(delta)))  
 end
